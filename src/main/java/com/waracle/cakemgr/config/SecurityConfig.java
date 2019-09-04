1c967f434c6eabfe2d6a909723eb1abaca99510c
david.bate <david.bate@bjss.com>
2019-09-04 15:44:51 +0100

    Added oauth2 authentication

---
+++ src/main/java/com/waracle/cakemgr/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.waracle.cakemgr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .oauth2Login();
+    }
+}
